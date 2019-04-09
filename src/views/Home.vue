91920bc6a7f4ea6b8b944f49e7f6570ac598aae0
ElijahModestov <modestovib@gmail.com>
2019-04-09 21:20:58 +0300

    frontend design added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,33 @@
 <template>
   <div class="home">
-    <form @submit.prevent="sendData">
-      <div class="form-control">
-      	<label for="name">Name</label>
-      	<input v-model="name" id="name" type="text">
-      </div>
-      <div class="form-control">
-      	<label for="email">E-mail</label>
-      	<input v-model="email" id="email" type="email">
-      </div>
-      <div class="form-control">
-      	<label for="address">Address</label>
-      	<input v-model="address" id="address" type="text">
-      </div>
-      <div class="form-control">
-      	<label>
-      	  Gender<br>
-      	  <span>Male <input v-model="gender" id="male" type="radio" value="male"></span>
-      	  <span>Female <input v-model="gender" id="female" type="radio" value="female"></span>
-        </label>
-      </div>
-      <input type="submit" class="send" value="Send">
-    </form>
+    <md-card id="card-home">
+      <form @submit.prevent="sendData" id="main-form">
+        <md-input-container class="form-control">
+        	<label>Name</label>
+        	<md-input v-model="name" id="name" type="text" />
+        </md-input-container>
+        
+        <md-input-container class="form-control">
+        	<label>E-mail</label>
+        	<md-input v-model="email" id="email" type="email" />
+        </md-input-container>
+        
+        <md-input-container class="form-control">
+        	<label>Address</label>
+        	<md-input v-model="address" id="address" type="text" />
+        </md-input-container>
+        
+        <div class="form-control">
+        	<label>Gender</label>
+        	<br>
+      	  <md-radio v-model="gender" id="male" md-value="male" class="md-primary">Male</md-radio>
+      	  <md-radio v-model="gender" id="female" md-value="female" class="md-primary">Female</md-radio>
+        </div>
+        <div class="send-wrapper">
+          <md-button type="submit" class="md-raised md-primary" id="send">Send</md-button>
+        </div>
+      </form>
+    </md-card>
   </div>
 </template>
 
@@ -58,6 +64,59 @@ export default {
 
 .send {
   margin: 5px;
+}
+
+.form-control:nth-child(4) {
+  padding-left: 0;
+}
+
+.form-control:nth-child(4) > label {
+  font-size: 17px;
+  color: rgba(0, 0, 0, 0.6);
+}
+
+.send-wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+#send {
+  margin-top: 5px;
+  margin-bottom: 10px;
+}
+
+#main-form {
+  padding: 5px;
+}
+
+.home {
+  width: 30%;
+  min-width: 250px;
+  opacity: 0;
+  position: absolute;
+  left: 50%;
+  top: -100%;
+  transform: translate(-50%);
+  animation-name: home;
+  animation-duration: .5s;
+  animation-timing-function: linear;
+  animation-fill-mode: forwards;
+}
+
+@keyframes home {
+  from {
+    opacity: 0;
+    top: -100%;
+  }
+  to {
+    opacity: 1;
+    top: 16%;
+  }
+}
+
+#card-home {
+  background-color: rgba(255, 255, 255, 0.6);
 }
 
 </style>
